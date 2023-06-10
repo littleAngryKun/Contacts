8622f3c032c5a55e749e9f2756ac5686dc52a0dc
Alex <1517056776@qq>
2023-06-10 20:03:25 +0800

    这是一个测试版本，都是报错

--- app/src/main/java/com/example/contacts_wb/Contact.java
+++ app/src/main/java/com/example/contacts_wb/Contact.java
@@ -5,6 +5,8 @@ import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
+import java.util.List;
+
 @Entity(tableName = "contact_table")
 public class Contact {
     @PrimaryKey
@@ -13,13 +15,26 @@ public class Contact {
     private String name;
     @ColumnInfo(name = "phonenumber")
     private String phonenumber;
-    public Contact(@NonNull String name,String phonenumber) {
+    @ColumnInfo(name = "sex")
+    private int sex;
+    @ColumnInfo(name = "callLog")
+    private List<CallLog> callLogs;
+    public Contact(@NonNull String name,String phonenumber,int sex,List<CallLog> callLogs) {
         this.name = name;
-        this.phonenumber = phonenumber;}
+        this.phonenumber = phonenumber;
+        this.sex=sex;
+        this.callLogs=callLogs;
+    }
     public String getName(){
         return this.name;
     }
     public String getPhonenumber(){
         return this.phonenumber;
+    }
+    public List<CallLog> getCallLogs() {
+        return callLogs;
+    }
+    public int getSex() {
+        return sex;
     }
 }
