f101276b2a346f5f63b9aadfc9c870122076ca8c
Dreamzero <1349803351@qq.com>
2023-06-12 21:00:18 +0800

    拨号界面的实现

---
+++ app/src/main/java/com/example/contacts_wb/dialing.java
@@ -0,0 +1,14 @@
+package com.example.contacts_wb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class dialing extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dialing);
+    }
+}
